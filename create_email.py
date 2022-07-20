505b39a10dda4686f2b7de7c7578a6e30db69a7a
Babage18 <106166493+Babage18@users.noreply.github.com>
2022-07-20 10:35:00 -0500

    Update create_email.py
    Change table design in HTML

--- create_email.py
+++ create_email.py
@@ -4,13 +4,14 @@ import os
 
 dict_email = {};
 Separation_Char = None;
+EmptySpace_Char = None;
 
 def getExcelData(workbook, sheetName):
     global Separation_Char
     global EmptySpace_Char
-    pxl_doc = openpyxl.load_workbook(workbook, data_only = True)
-    sheet = pxl_doc[sheetName]
-    excelData = {}
+    pxl_doc = openpyxl.load_workbook(workbook, data_only = True);
+    sheet = pxl_doc[sheetName];
+    excelData = {};
     # iterate over columns in the first row
     for i in range(1, sheet.max_column+1):
         excelData[sheet.cell(row=1, column=i).value] = sheet.cell(row=2, column=i).value;
@@ -22,75 +23,135 @@ def getExcelData(workbook, sheetName):
 
     return excelData
 
-def drawHTMLTable(col_1, col_2, col_3):
+def drawHTMLTable(col_1, col_2, col_3 = None, hyper_1 = None, hyper_2 = None, hyper_3 = None):
     global Separation_Char
+    global EmptySpace_Char
     col_1 = col_1.split(Separation_Char);
     col_2 = col_2.split(Separation_Char);
-    col_3 = col_3.split(Separation_Char);
+
+    if type(col_3) == type(None):
+        pass
+    
+    else:
+        col_3 = col_3.split(Separation_Char);
+
+    table = [];
+    table.append(col_1);
+    table.append(col_2);
+    table.append(col_3);
+
+    if type(hyper_1) == type(None):
+        pass
+    
+    else:
+        hyper_1 = hyper_1.split(Separation_Char);
+        hyper_1.insert(0, None);
+
+    if type(hyper_2) == type(None):
+        pass
+    
+    else:
+        hyper_2 = hyper_2.split(Separation_Char);
+        hyper_2.insert(0, None);
+
+    if type(hyper_3) == type(None):
+        pass
+    
+    else:
+        hyper_3 = hyper_3.split(Separation_Char);
+        hyper_3.insert(0, None);
+
+    hyper = [];
+    hyper.append(hyper_1);
+    hyper.append(hyper_2);
+    hyper.append(hyper_3);
 
     HTML_str = "<table>";
-    HTML_str = HTML_str + "<tr>";
-    HTML_str = HTML_str + "<th style = \"width: 30%\">{}</th><th>{}</th>".format(col_1[0], col_2[0], col_3[0]);
-    HTML_str = HTML_str + "</tr>"
-    for i in range(1, 3):
-        HTML_str = HTML_str + "<tr>";
-        HTML_str = HTML_str + "<td>{}</td><td>{}</td>".format(col_1[i], col_2[i], col_3[i]);
-        HTML_str = HTML_str + "</tr>"
+    
+    if type(col_3) == type(None):
+        for i in range(len(table[0])):
+            HTML_str = HTML_str + "<tr>";
+            if i == 0:
+                HTML_str = HTML_str + "<th width = \"70%\">{}&emsp;</th><th>{}</th>".format(table[0][i], table[1][i]);
 
+            else:
+                HTML_str = HTML_str + "<td>{}&emsp;</td><td>{}</td>".format(table[0][i], table[1][i]);
+                
+            HTML_str = HTML_str + "</tr>";
+
+    else:
+        for i in range(len(table[0])):
+            HTML_str = HTML_str + "<tr>";
+            for j in range(len(table)):
+                ## Place Headers in first definition of table
+                if i == 0:
+                    if j == 0:
+                        HTML_str = HTML_str + "<th style = \"width: 30%\">";
+
+                    elif j == 1:
+                        HTML_str = HTML_str + "<th>";
+
+                    elif j == 2:
+                        HTML_str = HTML_str + "<th style = \"width: 25%\">";
+
+                    HTML_str = HTML_str + "{}</th>".format(table[j][i]);
+                        
+                ## if headers have already been placed, continue as normal
+                else:
+                    HTML_str = HTML_str + "<td align = \"center\">";
+                    
+                    ## If no Hyperlink is provided in general, insert as text
+                    if type(hyper[j]) == type(None):
+                        HTML_str = HTML_str + "{}".format(table[j][i]);
+
+                    else:
+                        ## If no Hyperlink is provided for specific cell, insert as text
+                        if hyper[j][i] == EmptySpace_Char:
+                            HTML_str = HTML_str + "{}".format(table[j][i]);
+
+                        else:
+                            HTML_str = HTML_str + "<a href={}>{}".format(hyper[j][i], table[j][i]);
+                    
+            HTML_str = HTML_str + "</tr>";
+            
     HTML_str = HTML_str + "</table>";
 
-##    print(HTML_str);
     return HTML_str;
 
 def func_genSubmissionEmail(data, pptx_name):
-##    pptx_name = "NPI-Microchip-DSC612-613.pptx";
-
     data = getExcelData("./NPI_TEMPLATE_FILL_Test.xlsx", 'TEMPLATE_1_FILL')
 
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-######====================================================================
-####    msg = outlook.OpenSharedItem(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15\SubmissionEmail_Template.msg")
-######====================================================================
     msg = outlook.OpenSharedItem("{}\\{}".format(os.getcwd(), "SubmissionEmail_Template.msg"))
+##    print(msg.HTMLBody);
 
     msg.Subject = "{} {} - {}".format(msg.Subject, data['Supplier'], data["Title"]);
-    msg.Body = msg.Body.replace("Supplier:", "Supplier: " + data['Supplier'])
-    msg.Body = msg.Body.replace("Topic:", "Topic: " + data['Title'])
-    msg.Body = msg.Body.replace("Application:", "Application:\n" + data['ApplicationText'])
+    msg.HTMLBody = msg.HTMLBody.replace("<p><span class=SpellE>TableHere</span><o:p></o:p></p>",
+                                        "{}".format(drawHTMLTable(col_1 = data['OPNTableColumn1'], col_2 = data['OPNTableColumn4'])));
 
-######====================================================================
-####    msg.Attachments.Add(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15" + "\\{}".format(pptx_name));
-####    msg.Attachments.Add(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15\Cust_Email.msg");
-######====================================================================
+##    print(msg.HTMLBody);
+    msg.HTMLBody = msg.HTMLBody.replace("Supplier:", "Supplier: " + data['Supplier'])
+    msg.HTMLBody = msg.HTMLBody.replace("Topic:", "Topic: " + data['Title'])
+    msg.HTMLBody = msg.HTMLBody.replace("Application:", "Application:\n" + data['ApplicationText'])
+
     msg.Attachments.Add("{}\\{}".format(os.getcwd(), pptx_name));
-    msg.Attachments.Add("{}\\{}".format(os.getcwd(), "Cust_Email.msg"));
+    msg.Attachments.Add("{}\\{}".format(os.getcwd(), "{}.msg".format(pptx_name[0:pptx_name.find(".")])));
 
-######====================================================================
-####    msg.SaveAs(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15\NPI_email.msg", 9)
-######====================================================================
-    msg.SaveAs("{}\\{}".format(os.getcwd(), "NPI_email.msg"), 3)
+    msg.SaveAs("{}\\{}".format(os.getcwd(), "Submission-{}.msg".format(pptx_name[0:pptx_name.find(".")])), 3)
 
     del msg, outlook
 
-
-
 def func_genClientEmail(data, pptx_name):
-##    pptx_name = "NPI-Microchip-DSC612-613.pptx";
-
     data = getExcelData("./NPI_TEMPLATE_FILL_Test.xlsx", 'TEMPLATE_1_FILL')
 
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-######====================================================================
-####    msg = outlook.OpenSharedItem(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15\CustomerEmail_Template.msg")
-######====================================================================
     msg = outlook.OpenSharedItem("{}\\{}".format(os.getcwd(), "CustomerEmail_Template.msg"))
 
     msg.Subject = "{} - {}".format(data['Supplier'], data["Title"]);
-##    print(msg.HTMLBody)
+
     msg.Attachments.Add("{}\\{}".format(os.getcwd(), pptx_name));
-    msg.Attachments.Add("{}\\{}".format(os.getcwd(), data["MainFigureImage"]));
     msg.HTMLBody = msg.HTMLBody.replace("OverviewText", data['OverviewText'])
     msg.HTMLBody = msg.HTMLBody.replace("AdvantagesText", data['AdvantagesText'])
     msg.HTMLBody = msg.HTMLBody.replace("ApplicationText", data['ApplicationText'])
@@ -101,11 +162,8 @@ def func_genClientEmail(data, pptx_name):
         if msg.HTMLBody[width_index] == " ":
             break;
 
-##        print(msg.HTMLBody[width_index]);
         width_str = width_str + msg.HTMLBody[width_index];
         width_index += 1;
-
-##    print(width_str)
     
     height_index = msg.HTMLBody.rfind("height=") + len("height=");
     height_str = "";
@@ -113,64 +171,30 @@ def func_genClientEmail(data, pptx_name):
         if msg.HTMLBody[height_index] == " ":
             break;
 
-##        print(msg.HTMLBody[height_index]);
         height_str = height_str + msg.HTMLBody[height_index];
         height_index += 1;
 
-##    print(height_str)
-
     main_dir = os.getcwd();
-##    main_dir = main_dir.replace(" ", "%20");
-##    main_dir = main_dir.replace(",", "%2C");
-##    main_dir = main_dir.replace("\\", "/");
-    print(main_dir)
-    attachment = msg.Attachments.Add("{}\\images\\figures\\{}".format(main_dir, data['MainFigureImage']))
-    attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "imgId1")
     
-    msg.HTMLBody = msg.HTMLBody.replace("<img width={} height={} id=\"Picture_x0020_1\" src=\"cid:image001.jpg@01D89A90.88E97900\" alt=\"Image Placeholder - Uning\">".format(width_str, height_str),
+    print(main_dir);
+    attachment1 = msg.Attachments.Add("{}\\images\\figures\\{}".format(main_dir, data['MainFigureImage']))
+    attachment1.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "imgId1")
+
+    attachment2 = msg.Attachments.Add("{}\\images\\figures\\{}".format(main_dir, data['SubFigure1Image']))
+    attachment2.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "imgId2")
+    
+    msg.HTMLBody = msg.HTMLBody.replace("<img width={} height={} id=\"Picture_x0020_2\" src=\"cid:image001.jpg@01D89A90.88E97900\" alt=\"Image Placeholder - Uning\">".format(width_str, height_str),
                                         "<img width={} src=\'cid:{}\' alt=\'{}\'>".format(width_str, "imgId1", data['Supplier']))
+    msg.HTMLBody = msg.HTMLBody.replace("<img width={} height={} id=\"Picture_x0020_1\" src=\"cid:image001.jpg@01D89A90.88E97900\" alt=\"Image Placeholder - Uning\">".format(width_str, height_str),
+                                        "<img height={} src=\'cid:{}\' alt=\'{}\'>".format(height_str, "imgId2", data['Supplier']))
+    
     msg.HTMLBody = msg.HTMLBody.replace("<span style='color:black'>TableHere</span>",
-                                        "{}".format(drawHTMLTable(data['OPNTableColumn1'], data['OPNTableColumn2'], data['OPNTableColumn3'])));
+                                        "{}".format(drawHTMLTable(col_1 = data['OPNTableColumn1'], col_2 = data['OPNTableColumn2'], col_3 = data['OPNTableColumn3'], hyper_1 = data['OPNTableColumn1Link'], hyper_3 = data['OPNTableColumn3Link'])));
     msg.HTMLBody = msg.HTMLBody.replace("<span style='color:black'>PageLink1|PageLink2<o:p></o:p></span>",
-                                        "<a href={}>{}</a><FONT FACE=\"Calibri\"> |  </FONT><a href={}>{}</a>".format(data['PageLink1'], data['PageLink1Mask'],
-                                                                                                                                data['PageLink2'], data['PageLink2Mask']))
+                                        "<a href={}>{}</a><FONT FACE=\"Calibri\"> |  </FONT><a href={}>{}</a>".format(data['PageLink1'], data['PageLink1Mask'], data['PageLink2'], data['PageLink2Mask']))
 
-    print(msg.HTMLBody)
-####    msg.Body = msg.Body.replace("OverviewText", data['OverviewText'])
-####    msg.Body = msg.Body.replace("AdvantagesText", data['AdvantagesText'])
-####    msg.Body = msg.Body.replace("ApplicationText", data['ApplicationText'])
-######    msg.Body = msg.Body.replace("PageLink1", '{\field{\*\fldinst { HYPERLINK "{0}" }}{\fldrslt {{1}}}}'.format(data['PageLink1'], data['PageLink1Mask']).encode('utf-8'))
-######    Link1 = '{\field{\*\fldinst { HYPERLINK' +  '"' + data['PageLink1'] + '"' + '}}{\fldrslt {' + data['PageLink1Mask'] + '}}}';
-######    msg.Body = msg.Body.replace("PageLink1", Link1.encode('utf-8'))
-##########====================================================================
-########    msg.Attachments.Add(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15" + "\\{}".format(pptx_name));
-##########====================================================================
-####    msg.Attachments.Add("{}\\{}".format(os.getcwd(), pptx_name));
-######    print(msg.SenderName)
-######    print(msg.SenderEmailAddress)
-######    print(msg.SentOn)
-######    print(msg.To)
-######    print(msg.CC)
-######    print(msg.BCC)
-######    print(msg.Subject)
-######    print(msg.Body)
-####
-####    msg.HTMLBody = msg.HTMLBody.replace("<BR><FONT FACE=\"Calibri\">ImageHere </FONT>", "<BR><img src=\"{}\" alt=\"{}\">".format(os.getcwd() + "\\images\\figures\\" + data['MainFigureImage'], data['Supplier']))
-####    msg.HTMLBody = msg.HTMLBody.replace("<FONT FACE=\"Calibri\">PageLink1 | PageLink2 </FONT>",
-####                                        "<a href={}>{}</a><FONT FACE=\"Calibri\"> |  </FONT><a href={}>{}</a>".format(data['PageLink1'], data['PageLink1Mask'],
-####                                                                                                                                data['PageLink2'], data['PageLink2Mask']))
-####
-####    ##Table_data = data[""
-####    msg.HTMLBody = msg.HTMLBody.replace("<BR><FONT FACE=\"Calibri\">TableHere </FONT>", "{}".format(drawHTMLTable(data['OPNTableColumn1'], data['OPNTableColumn2'], data['OPNTableColumn3'])));
-##    msg.HTMLBody = msg.HTMLBody.replace("<FONT FACE=\"Calibri\"> </FONT>", "<BR>{}".format(drawHTMLTable(data['OPNTableColumn1'], data['OPNTableColumn2'], data['OPNTableColumn3'])));
-    ##image_index = msg.HTMLBody.rfind("</P>");
-    ##
-    ##msg.HTMLBody = msg.HTMLBody[:image_index+4] + "\n\n<BR><img src=\"{}\" alt=\"{}\">".format(r"C:\Users\146294\DSC613.PNG", data['Supplier']) + msg.HTMLBody[image_index+4:]
+##    print(msg.HTMLBody);
 
-##    print("\n\n")
-##    print(msg.HTMLBody)
+    msg.SaveAs("{}\\{}".format(os.getcwd(), "{}.msg".format(pptx_name[0:pptx_name.find(".")])), 3);
 
-######====================================================================
-####    msg.SaveAs(r"C:\Users\146294\OneDrive - Arrow Electronics, Inc\Documents\Arrow\Internal Jobs\NPI_Gen\NPI Generator Tool v15\Cust_email.msg", 3)
-######====================================================================
-    msg.SaveAs("{}\\{}".format(os.getcwd(), "Cust_email.msg"), 3)
+    del msg, outlook
